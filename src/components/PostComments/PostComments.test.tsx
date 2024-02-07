9c11b24e7ecdf53f86a74535ceb03776eb715294
CarolinaLunardi <lunardicll@gmail.com>
2024-02-07 15:17:22 +0000

    Testando a inserção de dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
-});
+    })
+    test('Deve renderizar dois comentários', () => {
+        render(<PostComment />)
+
+        fireEvent.change(screen.getByTestId('text-comment'),{
+            target: {
+                value: 'Primeiro comentário',
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comment'))
+        
+        fireEvent.change(screen.getByTestId('text-comment'),{
+            target: {
+                value: 'Segundo comentário',
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comment'))
+        
+        
+        expect(screen.getAllByTestId('element-comment')).toHaveLength(2)
+    })
+})
